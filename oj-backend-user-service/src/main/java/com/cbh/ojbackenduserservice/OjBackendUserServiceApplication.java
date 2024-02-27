d57aaf8b58af6d605a74b33617fd7b4d001a6761
xcc-cs-2020-2-2011030149 <1770194209@qq.com>
2024-02-27 22:27:40 +0800

    user-service 用户模块代码引入

--- oj-backend-user-service/src/main/java/com/cbh/ojbackenduserservice/OjBackendUserServiceApplication.java
+++ oj-backend-user-service/src/main/java/com/cbh/ojbackenduserservice/OjBackendUserServiceApplication.java
@@ -1,9 +1,20 @@
 package com.cbh.ojbackenduserservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+// todo 如需开启 Redis，须移除 exclude 中的内容
+//@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
 @SpringBootApplication
+@MapperScan("com.cbh.ojbackenduserservice.mapper")
+@EnableScheduling
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+// 扫包（不同模块需要扫包）
+@ComponentScan("com.cbh")
 public class OjBackendUserServiceApplication {
 
     public static void main(String[] args) {
---
+++ oj-backend-user-service/src/main/java/com/cbh/ojbackenduserservice/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.cbh.ojbackenduserservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cbh.ojbackendmodel.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author 小皮卡
+ * @from cbh
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
